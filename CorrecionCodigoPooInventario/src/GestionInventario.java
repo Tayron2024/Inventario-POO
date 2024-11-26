16a30eaef630811e6d8c3b78ea4db38c5910a61a
Sebaslsst <leonardo.sanchez@unl.edu.ec>
2024-11-26 08:33:16 -0500

    Delete CorrecionCodigoPooInventario directory

--- CorrecionCodigoPooInventario/src/GestionInventario.java
+++
@@ -1,6 +0,0 @@
-public interface GestionInventario {
-    void agregarItem(Insumo insumo);
-    void eliminarItem(Insumo insumo);
-    Insumo buscarItem(String nombre);
-    void actualizarInventario();
-}
